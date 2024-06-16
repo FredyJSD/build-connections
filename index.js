e66861a288259e5bc0c577aaac824cc2148d91ae
Fredy Santos <fredy0113@gmail.com>
2024-06-15 20:04:05 -0400

    Backend progress

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+async function loadJson(questionFile) {
+    try {
+        const questionJSON = await fs.readFile(`./${questionFile}.json`, 'utf-8');
+        return JSON.parse(questionJSON);
+    } catch (err) {
+        console.error('Error reading or parsing the JSON file:', err);
+        throw err; // Re-throw the error to handle it in the calling function
+    }
+}
+
 app.get("/", async (req, res) => {
     res.render("index.ejs");
 });
@@ -19,15 +29,40 @@ app.get("/menu", async (req, res) =>{
 });
 
 app.get("/icebreaker", async (req, res) =>{
-    res.render("ice.ejs");
+    const questions = await loadJson('questions');
+    const levels = questions.levels;
+    const iceLevel = levels.find(function(level){
+        return level.name === "ice breaker";
+    })
+    const iceQuestion = iceLevel.questions;
+    console.log(iceQuestion);
+    res.render("ice.ejs", {
+        question: iceQuestion
+    });
 });
 
 app.get("/confess", async (req, res) =>{
-    res.render("confess.ejs");
+    const questions = await loadJson('questions');
+    const levels = questions.levels;
+    const confessLevel = levels.find(function(level){
+        return level.name === "confess";
+    })
+    const confessQuestion = confessLevel.questions;
+    res.render("confess.ejs", {
+        question: confessQuestion
+    });
 });
 
 app.get("/deep", async (req, res) =>{
-    res.render("deep.ejs");
+    const questions = await loadJson('questions');
+    const levels = questions.levels;
+    const deepLevel = levels.find(function(level){
+        return level.name === "deep";
+    })
+    const deepQuestion = deepLevel.questions;
+    res.render("deep.ejs", {
+        question: deepQuestion
+    });
 });
 
 app.listen(port, () => {
